9029b4d9c1ada48da21c0f9b1d9642356536ab69
MiyoKouseki <controls@k1ctr5.kagra.icrr.u-tokyo.ac.jp>
2018-03-18 17:51:19 +0900

    add nds2 data taking function in mpio.py

--- miyopy/mpio.py
+++ miyopy/mpio.py
@@ -3,9 +3,50 @@
 import numpy as np
 from scipy import signal
 import mpplot as mpp
-
 from gwpy.timeseries import TimeSeries
 from glue.lal import Cache
+import nds2
+import numpy as np
+import pickle
+
+def fetch_data(start,end,chlst):
+    '''
+    NDS2を利用してデータを取得する。
+    
+    Parameters
+    ----------
+    start : int
+        Start GPS time.
+    end : int
+        End GPS time.
+    chlst : list
+        List of the CDS channel.
+    
+    Returns
+    -------
+    data : list 
+        
+
+    Example
+    -------
+    >>> channel = ['K1:PEM-EX1_SEIS_WE_SENSINF_OUT16']
+    >>> fetch_data(1205201472,1205205568,channel)
+    <list of the numpy array>
+    '''
+    conn = nds2.connection('10.68.10.121', 8088) # nds0
+    buffers = conn.fetch(start,end,chlst)
+    data = [b.data for b in buffers]
+    return data
+
+def dump(fn,data):
+    with open(fn, 'wb') as f:       
+        pickle.dump(data, f)
+
+def load(fn):
+    with open(fn, 'rb') as f:
+        data_ = pickle.load(f)
+    return data_
+
 
 
 class data(object):
