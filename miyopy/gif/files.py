5e08cfa040c058ee190a1c3f28d82640e10f0f1d
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-10-20 18:10:23 +0900

    Add duncan memo

--- miyopy/gif/files.py
+++ miyopy/gif/files.py
@@ -1,9 +1,10 @@
 #
 #! coding:utf-8
-from astropy import units as u
-import numpy as np
-import traceback
-import os
+import os 
+from astropy.time import Time
+from numpy import arange
+
+from datetime import datetime as dt
 
 
 def findfiles(cls,start,tlen,chname,prefix='/Volumes/HDPF-UT/DATA/'):
@@ -22,12 +23,51 @@ def findfiles(cls,start,tlen,chname,prefix='/Volumes/HDPF-UT/DATA/'):
 
     Return
     ------
-    flist: list of str
-        file path.
+    segment: list of list
+        Segment contains only list of file path, which files are exist. 
     '''
     _00sec = lambda gps: gps - (gps%60) + 18    
     _s = _00sec(start)
     _e = _00sec(start+tlen)
-    gpslist = np.arange(_s,_e+60,60)    
-    flist = [cls.path_to_file(chname,gps,prefix) for gps in gpslist]
-    return flist
+    gpslist = arange(_s,_e+60,60)    
+    segments = [[]]
+    for gps in gpslist:
+        path = cls.path_to_file(chname, gps, prefix)
+        if os.path.exists(path):
+            segments[-1].append(path)
+        else:
+            segments.append([])
+    if not segments[-1]:
+        segments.pop(-1)
+    return segments
+#flist = [cls.path_to_file(chname,gps,prefix) for gps in gpslist]
+#return flist
+
+def fname2gps(fname):
+    datetime_str = fname.split('.')[0].split('/')[-1]
+    time = dt.strptime(datetime_str, '%y%m%d%H%M')
+    gps = Time(time).gps
+    return gps
+
+
+def _read():
+    segments = findfiles()
+    data = []
+    times = []
+    for files in segments:
+        new = TimeSeries.read(files,)
+        data.append(new.value)
+        times.append(new.times.value)
+    alldata = numpy.concatenate(data)
+    alltimes = numpy.concatenate(times)
+    # interporate
+    
+
+def _read_with_nan():
+    allfiles = [path for files in segments for path in files]
+    data = TimeSeries.read(allfiles, pad=numpy.nan)
+    spectrogram = data.spectrogram()
+    badtimes = numpy.isnan(spectrogram[0,:])
+    gooddata = spectrogram.where(badtimes)
+    asd = gooddata.percentile(50)
+    
