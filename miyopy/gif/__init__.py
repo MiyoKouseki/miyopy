75c58b9223aa7c5fccb7ceb4d381dfecafa2afdd
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2018-10-21 20:38:49 +0900

    Use KagraStrain

--- miyopy/gif/__init__.py
+++ miyopy/gif/__init__.py
@@ -3,5 +3,6 @@
 #from .read import read
 #from .findfiles import check_filesize, path_to_file, findFiles, fromfiles, check_nan
 from .datatype import GifData
-from .files import findfiles,fname2gps
+from .files import findfiles,fname2gps,gps2datestr
 from .rotation import rotate2d
+from .gotic import KagraStrain
