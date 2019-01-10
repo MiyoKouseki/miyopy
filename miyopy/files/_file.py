f1bb7eeb8360cf9722a24b11e60a726243676043
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-01-10 13:30:40 +0900

    Update files/_file.py

--- miyopy/files/_file.py
+++ miyopy/files/_file.py
@@ -12,8 +12,8 @@ def get_csd_specgram(chname1,chname2,remake=True,fftlength=2**6,overlap=0.0,**kw
     if remake:
         if os.path.exists(hdf5fname):
             os.remove(hdf5fname)
-        timeseries1 = get_timeseries(chname1,nds=False,**kwargs)
-        timeseries2 = get_timeseries(chname2,nds=False,**kwargs)
+        timeseries1 = get_timeseries(chname1,**kwargs)
+        timeseries2 = get_timeseries(chname2,**kwargs)
         specgram = timeseries1.csd_spectrogram(timeseries2,
                                                stride=fftlength*2,
                                                fftlength=fftlength,
@@ -32,7 +32,7 @@ def get_asd(chname,**kwargs):
     fftlength = kwargs.pop('fftlength',2**7)
     remake = kwargs.pop('remake',False)
     overlap = kwargs.pop('overlap',None)    
-    timeseries = get_timeseries(chname,nds=False,**kwargs)    
+    timeseries = get_timeseries(chname,**kwargs)    
     asd = timeseries.asd(fftlength=fftlength,
                          overlap=overlap,
                          window='hanning')
@@ -42,8 +42,8 @@ def get_csd(chname1,chname2,**kwargs):
     fftlength = kwargs.pop('fftlength',2**7)
     remake = kwargs.pop('remake',False)
     overlap = kwargs.pop('overlap',None)    
-    timeseries1 = get_timeseries(chname1,nds=False,**kwargs)
-    timeseries2 = get_timeseries(chname2,nds=False,**kwargs)    
+    timeseries1 = get_timeseries(chname1,**kwargs)
+    timeseries2 = get_timeseries(chname2,**kwargs)    
     csd = timeseries1.csd(timeseries2,
                           fftlength=fftlength,
                           overlap=overlap,
@@ -55,8 +55,8 @@ def get_coherence(chname1,chname2,**kwargs):
     fftlength = kwargs.pop('fftlength',2**7)
     remake = kwargs.pop('remake',False)
     overlap = kwargs.pop('overlap',None)    
-    timeseries1 = get_timeseries(chname1,nds=False,**kwargs)
-    timeseries2 = get_timeseries(chname2,nds=False,**kwargs)    
+    timeseries1 = get_timeseries(chname1,**kwargs)
+    timeseries2 = get_timeseries(chname2,**kwargs)    
     coherence = timeseries1.coherence(timeseries2,
                                       fftlength=fftlength,
                                       overlap=overlap,
@@ -80,16 +80,16 @@ def get_specgram(*chname,**kwargs):
     remake = kwargs.pop('remake',False)
     overlap = kwargs.pop('overlap',None)
     hdf5fname = to_hdf5fname(chname,**kwargs)
-    remake = kwargs.pop('remake',False)
+    #print hdf5fname
     if remake:
         if os.path.exists(hdf5fname):
             os.remove(hdf5fname)
-        print kwargs
         timeseries = get_timeseries(chname,**kwargs)
         specgram = timeseries.spectrogram(stride=fftlength*2,
                                           fftlength=fftlength,
                                           overlap=overlap,
                                           window='hanning')
+        #exit()        
         specgram.write(hdf5fname)
         return specgram
     else:
@@ -100,24 +100,22 @@ def get_specgram(*chname,**kwargs):
 
 
 
-def get_timeseries(chname,prefix='./data',nds=False,**kwargs):
-    start = kwargs.pop('start')
-    end = kwargs.pop('end')
-    fname = to_gwffname(chname,**kwargs)    
-    if not nds and os.path.exists(fname):
-        #print('!')
-        #print fname
-        #exit()
+def get_timeseries(chname,**kwargs):
+    start = kwargs.pop('start',None)
+    end = kwargs.pop('end',None)
+    nds = kwargs.pop('nds',None)
+    fname = to_gwffname(chname,**kwargs)
+
+    if os.path.exists(fname):
+        print '{0} exist.',format(fname)
         print('Skip fetch from nds {0}'.format(fname))
         data = TimeSeries.read(fname,chname,start,end,
                                 format='gwf.lalframe',
                                 verbose=True,
                                 pad=np.nan)
-        
         return data
-    else:
-        #print('!')
-        #exit()
+    elif nds and not os.path.exists(fname):
+        print('{0} dose not exist.',format(fname))
         data = TimeSeries.fetch(chname, start, end,
                                 verbose=True,
                                 host='10.68.10.121', port=8088,
@@ -126,14 +124,19 @@ def get_timeseries(chname,prefix='./data',nds=False,**kwargs):
         data.write(fname,format='gwf.lalframe')
         print('wrote data in '+fname)
         return data
+    else:
+        print nds
+        print os.path.exists(fname)
+        print fname
+        raise ValueError('! Must use nds or load files')
+        exit()
 
 
 
 
-def to_gwffname(chname,prefix='./data/'):
-    print(chname)
-    #m = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname)
-    m = re.search(r'K1:PEM-(.*)_OUT_DQ',chname)    
+def to_gwffname(chname,**kwargs):
+    prefix = kwargs.pop('prefix',None)
+    m = re.search(r'K1:PEM-(.*)_(.*)_DQ',chname)
     try:
         fname = prefix + m.group(1).lower() + '.gwf'
     except Exception as e:
@@ -149,13 +152,13 @@ def to_hdf5fname(*args,**kwargs):
         chname1,chname2 = args
         #m1 = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname1)
         #m2 = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname2)
-        m1 = re.search(r'K1:PEM-(.*)_OUT_DQ',chname1)
-        m2 = re.search(r'K1:PEM-(.*)_OUT_DQ',chname2)        
+        m1 = re.search(r'K1:PEM-(.*)_(.*)_DQ',chname1)
+        m2 = re.search(r'K1:PEM-(.*)_(.*)_DQ',chname2)
         fname = prefix + m1.group(1).lower() + '-' + m2.group(1).lower() + '.hdf5'
     elif N==1:
         chname = args[0]
         #m = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname)
-        m = re.search(r'K1:PEM-(.*)_OUT_DQ',chname)        
+        m = re.search(r'K1:PEM-(.*)_(.*)_DQ',chname)        
         try:
             fname = prefix + m.group(1).lower() + '.hdf5'
         except Exception as e:
@@ -169,7 +172,7 @@ def to_pngfname(chname,ftype,prefix='./',**kwargs):
         return  'None.png'
     
     #m = re.search(r'K1:PEM-(.*)_SENSINF_IN1_DQ',chname) old
-    m = re.search(r'K1:PEM-(.*)_OUT_DQ',chname)
+    m = re.search(r'K1:PEM-(.*)_(.*)_DQ',chname)
     try:
         fname = prefix + ftype + '_' + m.group(1).lower() + '.png'
     except Exception as e:
