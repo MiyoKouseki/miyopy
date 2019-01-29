1d7954d05df45929f55fcb4b3d994d34b4ed4af0
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-01-29 12:20:36 +0900

    Update

--- miyopy/example/gwpy/read_timeseries_with_lalframe.py
+++ miyopy/example/gwpy/read_timeseries_with_lalframe.py
@@ -10,6 +10,7 @@ source = 'K-K1_C-1231133824-32.gwf'
 chname = 'K1:PEM-IXV_GND_TR120Q_X_OUT_DQ'
 data = TimeSeries.read(source,chname,format='gwf.lalframe')
 data.override_unit('um/s') # bugs when use lalframe reader
+print(data)
 plot = data.plot()
 plot.savefig('result_timeseries.png')
 plot.close()
