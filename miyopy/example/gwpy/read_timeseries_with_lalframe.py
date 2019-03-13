36333d096e4c1dfb93e8f2a88361d102822b9693
MiyoKouseki <miyo@icrr.u-tokyo.ac.jp>
2019-03-13 15:38:07 +0900

    Update

--- miyopy/example/gwpy/read_timeseries_with_lalframe.py
+++ miyopy/example/gwpy/read_timeseries_with_lalframe.py
@@ -6,8 +6,8 @@ __author__ = "Koseki Miyo"
 
 from gwpy.timeseries import TimeSeries
 
-source = 'K-K1_C-1231133824-32.gwf'
-chname = 'K1:PEM-IXV_GND_TR120Q_X_OUT_DQ'
+source = '/frame0/full/12364/K-K1_C-1236493984-32.gwf'
+chname = 'K1:PEM-SEIS_IXV_GND_X_OUT_DQ'
 data = TimeSeries.read(source,chname,format='gwf.lalframe')
 data.override_unit('um/s') # bugs when use lalframe reader
 print(data)
--- miyopy/example/gwpy/read_timeseries_with_nds.py
+++ miyopy/example/gwpy/read_timeseries_with_nds.py
@@ -6,10 +6,9 @@ __author__ = "Koseki Miyo"
 
 from gwpy.timeseries import TimeSeries
 
-
-start = '2019 Jan 10 05:36:46'
-end = '2019 Jan 10 06:07:18'
-chname = 'K1:PEM-IXV_GND_TR120Q_X_OUT_DQ'
+start = '2019 Mar 10 05:36:46'
+end = '2019 Mar 10 06:07:18'
+chname = 'K1:PEM-SEIS_IXV_GND_X_OUT_DQ'
 data = TimeSeries.fetch(chname,start,end,host='k1nds0',port=8088)
 print(data)
 plot = data.plot()
